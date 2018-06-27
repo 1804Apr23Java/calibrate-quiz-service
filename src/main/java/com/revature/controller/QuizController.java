acb70f8febce72160ec7f65b923a97d000b6a5cd
ianH92 <ianhdrummond1992@gmail.com>
2018-06-27 13:46:18 -0700

    Changes to AnswerDTO

--- src/main/java/com/revature/controller/QuizController.java
+++ src/main/java/com/revature/controller/QuizController.java
@@ -1,6 +1,7 @@
 package com.revature.controller;
 
 import java.util.HashSet;
+import com.revature.DTO.AnswerDTO;
 import com.revature.DTO.QuestionDTO;
 import java.util.List;
 import java.util.Set;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.DTO.QuizDTO;
 import com.revature.beans.Quiz;
+import com.revature.service.AnswerService;
 import com.revature.service.QuestionService;
 import com.revature.service.QuizService;
 
@@ -31,12 +33,27 @@ public class QuizController {
 	@Autowired 
 	private QuizService quizService;
 	
+	@Autowired
+	private AnswerService answerService;
+	
 	@GetMapping("/generate")
 	public ResponseEntity<QuizDTO> generateQuiz(@RequestParam List<Integer> libraryIds, @RequestParam String name, @RequestParam int numQuestions) {
 		Quiz q = quizService.generateQuiz(name, libraryIds, numQuestions);
+		
 		Set<QuestionDTO> ggg = new HashSet<QuestionDTO>();
-		q.getQuestions().forEach((e) -> { ggg.add(new QuestionDTO(e)); });
- 		QuizDTO qq = new QuizDTO(q.getId(), q.getName(), ggg);
+		q.getQuestions().forEach((e) -> {
+			QuestionDTO z = new QuestionDTO(e);
+			
+			Set<AnswerDTO> as = new HashSet<>();
+			answerService.getAnswersByQuestion(e).forEach((l) -> {
+				as.add(new AnswerDTO(l));
+			});
+			z.setAnswers(as);
+			ggg.add(z); 
+		
+		});
+ 		
+		QuizDTO qq = new QuizDTO(q.getId(), q.getName(), ggg);
 		return new ResponseEntity<QuizDTO>(qq, HttpStatus.OK);
 	}
 	
@@ -44,7 +61,18 @@ public class QuizController {
 	public ResponseEntity<QuizDTO> getQuizById(@PathVariable int id) {
 		Quiz q = quizService.getQuiz(id);
 		Set<QuestionDTO> g = new HashSet<>();
-		q.getQuestions().forEach((e) -> { g.add(new QuestionDTO(e)); });
+		
+		q.getQuestions().forEach((e) -> { 
+			QuestionDTO p = new QuestionDTO(e);
+			
+			Set<AnswerDTO> as = new HashSet<>();
+			answerService.getAnswersByQuestion(e).forEach((l) -> {
+				as.add(new AnswerDTO(l));
+			});
+			p.setAnswers(as);
+			g.add(new QuestionDTO(e)); 
+		
+		});
 		
 		QuizDTO asd = new QuizDTO(q.getId(), q.getName(), g);
 		return new ResponseEntity<QuizDTO>(asd, HttpStatus.OK);
